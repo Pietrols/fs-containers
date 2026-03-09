adfd4233bd3f25d8825579c831f0cc5642335c84
Peter Kabamba <kabambapeter3@gmail.com>
2026-03-09 21:35:13 +0200

    add entry point to run server

---
+++ todo-app/todo-backend/server.js
@@ -0,0 +1,8 @@
+const app = require("./app");
+
+const PORT = process.env.PORT || 3000;
+
+// Bind to 0.0.0.0 so Docker can expose it
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
